8b789823d0298b82afa27d9fd41055544ac32feb
Rigaudeau Léo <rigauleo@gmail.com>
2018-03-13 09:41:43 +0100

    exercice 1 fini

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,16 @@
 var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+  
+  if (values===undefined) {
+    return output=[]
+  } else {
+    var output=[]
+    for (let index = 0; index < values.length; index++) {
+      if (values[index]>=0) {
+        output.push(values[index]);
+      }
+    }
+    return output 
+  }
 }
 
 module.exports = {
